b9d82931d9b83ea3822754d3bdb0c0c0c27d955a
Sgaykie0677 <4cmontgomery@gmail.com>
2026-02-25 15:24:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,18 @@
 --------------------------------*/
 const sections = [
   "home",
+  const sections = [
+  "home",
   "module1",
   "module2",
   "module3",
   "module4",
   "module5",
   "module6",
+  "module7", 
+  "module8", 
+  "module9", 
+  "module10", 
   "finalExam"
 ];
 
